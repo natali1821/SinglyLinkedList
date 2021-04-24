49904ed144f5222bf0ddd16f5fa817e66d17e637
natali1821 <ermolina_natali@bk.ru>
2021-04-24 16:56:35 +0300

    All tests are passed

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "SinglyLinkedList.h"
+
+int increment(int a) {
+	return ++a;
+}
+
+bool evenNumber(int a) {
+	return (a % 2 == 0);
+}
+
+int main()
+{
+	SLL first;
+	first = SLL();
+	first.insert(0, 1);
+	first.insert(1, 3);
+	first.insert(1, 2);
+	first.insert(3, 4);
+	first.pushBack(5);
+	first.pushFront(0);
+	std::cout << first.size() << std::endl;
+	first.print();
+
+	SLL second(first);
+	second.filter(evenNumber);
+	std::cout << second.size() << std::endl;
+	second.print();
+
+	SLL third = first.map(increment);
+	std::cout << third.size() << std::endl;
+	third.print();
+}
+

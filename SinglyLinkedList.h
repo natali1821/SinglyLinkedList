f9fdfadb4bcc0f3675cf50c617b6382a16d94f8b
natali1821 <ermolina_natali@bk.ru>
2021-05-08 18:49:01 +0300

    Add class Iterator

--- SinglyLinkedList.h
+++ SinglyLinkedList.h
@@ -1,9 +1,44 @@
 #pragma once
 #include <iostream>
 #include <utility>
+#include <exception>
+#include <cstdlib>
+
+using ValueType = int;
 
 class SLL {
+private:
+	class Node {
+	public:
+		Node* _next;
+		ValueType _data;
+		Node(ValueType data) {
+			_data = data;
+			_next = nullptr;
+		}
+	};
+	Node* _head;
+	size_t _size;
 public:
+	class Iterator {
+	public:
+		using iterator_category = std::forward_iterator_tag;
+		using difference_type	= std::ptrdiff_t;
+		using value_type		= ValueType;
+		using pointer			= ValueType*;
+		using reference			= ValueType&;
+		Iterator(Node* ptr);
+		reference operator*();
+		pointer operator->();
+		Iterator& operator++(); //prefix
+		Iterator operator++(int); //postfix
+		bool operator!=(const Iterator& other);
+		bool operator==(const Iterator& other);
+		difference_type operator-(const Iterator& other);
+	private:
+		Node* _ptr;
+	};
+
 	SLL();
 
 	//the rule of five
@@ -16,9 +51,9 @@ public:
 	~SLL();
 
 	//methods
-	void insert(size_t idx, const int& value);
-	void pushBack(const int& value);
-	void pushFront(const int& value);
+	void insert(size_t idx, const ValueType& value);
+	void pushBack(const ValueType& value);
+	void pushFront(const ValueType& value);
 
 	void clear();
 	void erase(size_t idx);
@@ -29,21 +64,10 @@ public:
 	void print();
 	bool isEmpty() const;
 
-	void forEach(int (*fn)(int));
-	SLL map(int (*fn)(int));
-	void filter(bool (*fn)(int));
+	void forEach(ValueType (*fn)(ValueType));
+	SLL map(ValueType (*fn)(ValueType));
+	void filter(bool (*fn)(ValueType));
 
-
-private:
-	class Node {
-	public:
-		Node* _next;
-		int _data;
-		Node(int data) {
-			_data = data;
-			_next = nullptr;
-		}
-	};
-	Node* _head;
-	size_t _size;
+	Iterator begin() const;
+	Iterator end() const;
 };
